18da88dda0362273a8d7f2b73603c8722b3bc10b
Mauro-Pereira <mauropereira1096@gmail.com>
2024-10-24 13:00:45 -0300

    feat: add foot component

---
+++ src/app/foot/foot.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foot',
+  standalone: true,
+  imports: [],
+  templateUrl: './foot.component.html',
+  styleUrl: './foot.component.css'
+})
+export class FootComponent {
+
+}
