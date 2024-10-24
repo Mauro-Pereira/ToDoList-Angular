916cb0e815c1b52b95297e119b19bd14b3887661
Mauro-Pereira <mauropereira1096@gmail.com>
2024-10-24 16:03:40 -0300

    feat: adding login component style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FootComponent } from './foot/foot.component';
+import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, 
+    RouterOutlet,
     HeaderComponent,
     FootComponent,
+    LoginComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
