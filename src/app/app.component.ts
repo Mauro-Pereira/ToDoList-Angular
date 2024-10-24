858966a83975d4fa11b710a61fd753e95d62e76f
Mauro-Pereira <mauropereira1096@gmail.com>
2024-10-24 11:07:52 -0300

    change: removing todo component and update packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from './todo/todo.component';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
