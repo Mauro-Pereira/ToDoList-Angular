1f2162d25d73160683b6e769885ef849935c0fce
Mauro-Pereira <mauropereira1096@gmail.com>
2024-10-24 13:10:03 -0300

    feat: adding html and css code in foot component and import it in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FootComponent } from './foot/foot.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent,
+    FootComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
