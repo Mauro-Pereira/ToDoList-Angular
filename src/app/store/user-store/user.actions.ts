5deb8a85b3fbb85b5aef593ea281ba41881ab546
Mauro-Pereira <mauropereira1096@gmail.com>
2025-01-31 14:50:31 -0300

    feat: adding store folder

---
+++ src/app/store/user-store/user.actions.ts
@@ -0,0 +1,17 @@
+import { User } from '../../model/user/user';
+
+export const loadUsers = createAction('[User] Load Users');
+export const loadUsersSuccess = createAction('[User] Load Users Success', props<{ users: User[] }>());
+export const loadUsersFailure = createAction('[User] Load Users Failure', props<{ error: any }>());
+
+export const addUser = createAction('[User] Add User', props<{ user: User }>());
+export const addUserSuccess = createAction('[User] Add User Success', props<{ user: User }>());
+export const addUserFailure = createAction('[User] Add User Failure', props<{ error: any }>());
+
+export const updateUser = createAction('[User] Update User', props<{ user: User }>());
+export const updateUserSuccess = createAction('[User] Update User Success', props<{ user: User }>());
+export const updateUserFailure = createAction('[User] Update User Failure', props<{ error: any }>());
+
+export const deleteUser = createAction('[User] Delete User', props<{ id: string }>());
+export const deleteUserSuccess = createAction('[User] Delete User Success', props<{ id: string }>());
+export const deleteUserFailure = createAction('[User] Delete User Failure', props<{ error: any }>());
