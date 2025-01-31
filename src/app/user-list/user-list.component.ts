970af93d56ecd530bf96e3430cfee76c8fc64241
Mauro-Pereira <mauropereira1096@gmail.com>
2025-01-31 20:01:32 -0300

    feat: add user-list implementation

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/user/user';
+import * as UserActions from '../store/user-store/user.actions';
+import { Store } from '@ngrx/store';
+import { selectLoading, selectUsers } from '../store/user-store/user.selectors';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-user-list',
-    imports: [],
+    standalone: true,
+    imports: [CommonModule],
     templateUrl: './user-list.component.html',
     styleUrl: './user-list.component.css'
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+
+    user$: Observable<User[]>;
+    loading$: Observable<boolean>;
+
+    constructor(private store: Store) {
+        this.user$ = this.store.select(selectUsers);
+        this.loading$ = this.store.select(selectLoading);
+    }
+
+    ngOnInit(): void {
+        this.store.dispatch(UserActions.loadUsers());
+    }
 
 }
