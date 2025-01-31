6926167b8b91d649c55ec8bb4dd50301478b6a56
Mauro-Pereira <mauropereira1096@gmail.com>
2025-01-31 15:24:51 -0300

    feat: making RGRX configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,24 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideHttpClient } from '@angular/common/http';
+import { UserEffects } from './store/user-store/user.effects';
+import { TaskEffects } from './store/task-store/task.effects';
+import { taskReducer } from './store/task-store/task.reducer';
+import { userReducer } from './store/user-store/user.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideEffects([UserEffects, TaskEffects]),
+    provideStore({
+      users: userReducer,
+      tasks: taskReducer
+    }),
+    provideHttpClient()
+  
+  ]
 };
