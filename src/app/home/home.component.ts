630fe92c4207df3d24ebd17ec5a1a37427ecf095
Mauro-Pereira <mauropereira1096@gmail.com>
2025-01-31 18:49:30 -0300

    feat: configuring modal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
+import { TaskModalComponent } from "../task-modal/task-modal.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [],
+    imports: [TaskModalComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  isModalOpen: boolean = false;
+  isEditMode: boolean = false;
+  selectedTask: any = { title: '', expirationDate: '', description: '' };
+  tasks: any[] = [];
+
+  // Abre o modal para criar uma nova tarefa
+  openModal() {
+    this.isModalOpen = true;
+    this.isEditMode = false;
+    this.selectedTask = { title: '', expirationDate: '', description: '' };
+  }
+
+  // Abre o modal para editar uma tarefa existente
+  editTask(task: any) {
+    this.isModalOpen = true;
+    this.isEditMode = true;
+    this.selectedTask = { ...task };
+  }
+
+  // Fecha o modal
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  // Manipula o envio do formulário
+  handleSubmit(task: any) {
+    if (this.isEditMode) {
+      // Atualiza a tarefa existente
+      const index = this.tasks.findIndex((t) => t === this.selectedTask);
+      this.tasks[index] = task;
+    } else {
+      // Adiciona uma nova tarefa
+      this.tasks.push(task);
+    }
+    this.closeModal();
+  }
 
 
 
--- src/app/task-modal/task-modal.component.ts
+++ src/app/task-modal/task-modal.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-modal',
-  imports: [],
+  standalone:true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './task-modal.component.html',
   styleUrl: './task-modal.component.css'
 })
 export class TaskModalComponent {
+
+  @Input() isModalOpen: boolean = false;
+  @Input() isEditMode: boolean = false;
+  @Input() task: any = { title: '', expirationDate: '', description: '' };
+  @Output() close = new EventEmitter<void>();
+  @Output() submit = new EventEmitter<any>();
+
+  taskForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.taskForm = this.fb.group({
+      title: [this.task.title, Validators.required],
+      expirationDate: [this.task.expirationDate, Validators.required],
+      description: [this.task.description],
+    });
+  }
+
+  closeModal() {
+    this.close.emit();
+  }
+
+  onSubmit() {
+    if (this.taskForm.valid) {
+      this.submit.emit(this.taskForm.value);
+    }
+  }
 
 }
