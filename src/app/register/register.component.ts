e0b1a9e4177a321932a3d85b2b2a89959379f07d
Mauro-Pereira <mauropereira1096@gmail.com>
2025-01-31 19:21:54 -0300

    feat: implementing  fuctionality of addUser

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import * as UserActions from '../store/user-store/user.actions';
+import { Store } from '@ngrx/store';
 
 @Component({
     selector: 'app-register',
     standalone: true,
-    imports: [],
+    imports: [ReactiveFormsModule],
     templateUrl: './register.component.html',
     styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+    userForm: FormGroup;
+
+    constructor(private fb: FormBuilder, private store: Store) {
+        this.userForm = this.fb.group({
+        name: ['', Validators.required, Validators.maxLength(50)],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        });
+
+    }
+
+    onSubmit(){
+
+        if(this.userForm.valid){
+            this.store.dispatch(UserActions.addUser({user: this.userForm.value}));
+        }
+
+    }
 
 }
