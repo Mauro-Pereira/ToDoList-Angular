b708373715cd2bc14305d759e90c4f7317900f89
Mauro-Pereira <mauropereira1096@gmail.com>
2025-01-31 16:17:32 -0300

    fix: fixing apiURL

--- src/app/services/task/task.service.ts
+++ src/app/services/task/task.service.ts
@@ -2,29 +2,34 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Task } from '../../model/task/task';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../../model/user/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  
-  private apiUrl = 'http://localhost:8080/api/tasks';
+
+  private apiUrl = 'http://localhost:8080/task';
 
   constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(this.apiUrl);
+    return this.http.get<Task[]>(`${this.apiUrl}/listAllTask`);
   }
 
-  addTask(task: Task): Observable<Task> {
-    return this.http.post<Task>(this.apiUrl, task);
+  getMyTasks(userId: string): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiUrl}/getMyTasks/${userId}`);
   }
 
-  updateTask(task: Task): Observable<Task> {
-    return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task);
+  addTask(task: Task, userId: string): Observable<Task> {
+    return this.http.post<Task>(`${this.apiUrl}/registerTask/${userId}`, task);
   }
 
-  deleteTask(id: string): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  updateTask(userId: string, taskId: string, task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/updateTask/${userId}/${taskId}`, task);
+  }
+
+  deleteTask(userId: string, taskId: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/deleteUserTask/${userId}/${taskId}`);
   }
 }
